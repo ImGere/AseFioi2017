7e573b9647039647f2be22cbdde53db8e325e4a7
Paolo Coba <paolo.coba@studenti.unitn.it>
2017-11-17 20:55:12 +0100

    Fixed layout of new Client and Hour views

--- Assignments/AppFatturazione/config/routes.rb
+++ Assignments/AppFatturazione/config/routes.rb
@@ -1,21 +1,19 @@
 Rails.application.routes.draw do
-  resources :hours
-  resources :clients
-  resources :users
+
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to:'users#new'
-  post 'signup', to:'users#create'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   get '/client', to: 'clients#index'
   get '/client/new', to: 'clients#new'
   post '/client/new', to: 'clients#create'
-  get '/user/:id/edit', to: 'users#edit'
-  put '/user/:id', to: 'users#update'
+  resources :hours
+  resources :clients
+  resources :users, except: :new
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
