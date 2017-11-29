b4edc27837d3409df7d4bc4d581271461b4169ff
Paolo Coba <paolo.coba@studenti.unitn.it>
2017-11-29 18:48:15 +0100

    Fixed minor bug

--- Assignments/AppFatturazione/features/step_definitions/create_invoice_steps.rb
+++ Assignments/AppFatturazione/features/step_definitions/create_invoice_steps.rb
@@ -2,7 +2,7 @@ When("I select the hours I want to bill") do
   page.check("hour_ids_")
 end
 Then("My invoice should be created successfully") do
-  expect(page).to have_content("Print PDF version")
+  expect(page).to have_content("Create PDF document")
 end
 
 Then("I should get {string}")do |string|
